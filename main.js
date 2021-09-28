e781e0a50ff132f3cd3125d652868e7f5cf78fe1
bautipietra <bautistapietraroia@gmail.com>
2021-09-27 21:59:45 -0300

    first commit

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
-let usuario = prompt(`Hola! Ingresa tu nombre de usuario para continuar`);
-let numero1 = Number(prompt(`Ingresa el primer numero`));
-let numero2 = Number(prompt(`Ingresa otro numero`));
+let numero = Number(prompt(`Ingresa un numero menor a 20 y te mostraremos los numeros que hay en el medio`));
 
-let operacion = prompt(`Ingresa el simbolo de la operacion a realizar: '+' para sumar, '-' para restar, '*' para multiplicar y '/' para dividir`);
-
-if (operacion == `+`) {
-    alert(`Hola ${usuario}, la suma de ${numero1} y ${numero2} dan como resultado ` + (numero1 + numero2));
-} 
-else if (operacion == `-`) {
-    alert(`Hola ${usuario}, la resta de ${numero1} y ${numero2} dan como resultado ` + (numero1 - numero2));
-}
-else if (operacion == `*`) {
-    alert(`Hola ${usuario}, la multiplicacion de ${numero1} y ${numero2} dan como resultado ` + (numero1 * numero2));
-}
-else if (operacion == `/`) {
-    alert(`Hola ${usuario}, la division de ${numero1} y ${numero2} dan como resultado ` + (numero1 / numero2));
+if (numero < 10) {
+    for (numero; numero < 25; numero++) {
+        console.log(numero);
+    }
 }
 else {
-    alert(`Lo sentimos ${usuario}, el simbolo ingresado no es valido.`);
+    console.log(`El numero ingresado es mayor a 25`)
 }
